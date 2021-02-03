235a8fd86870a4c4cc195956f42e34f71954dac3
Mahfuz <you@example.com>
2021-02-03 12:22:15 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,33 @@
+// undefined value 
+
+// technique -1
+// let mahfuz;
+// console.log(mahfuz);
+
+
+// technique-2
+// function add(x,y){
+//     console.log(x+y);
+//     return
+// }
+// var result=add(12,15);
+// console.log(result);
+
+// technique-3
+// function addition(num1,num2){
+//     console.log(num2);
+// }
+
+// var result2=addition(12,15);
+// console.log(result2);
+
+// technique-4
+
+// let friends={name:"mahfuz",age:25,address:"dhaka"};
+// console.log(friends.gfName);
+
+
+//Null value
+
+// null value nije nije set kore dawa jai
+
